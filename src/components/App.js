3ed39559ce3f8691cd5b9f6fe7402d6c2a157f72
Leo Spratt <enchant97@users.noreply.github.com>
2021-11-28 14:08:36 +0000

    add todo deletion

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,19 @@ class App extends React.Component {
     super(props);
 
     this.handleTodoChange = this.handleTodoChange.bind(this);
+    this.handleTodoDelete = this.handleTodoDelete.bind(this);
 
     this.state = {
       todoList: []
     }
+  }
+
+  findTodoById(todoId) {
+    for (let i = 0; i < this.state.todoList.length; i++) {
+      const element = this.state.todoList[i];
+      if (element.id === todoId) { return i; }
+    }
+    return -1;
   }
 
   handleTodoChange(title, description) {
@@ -25,12 +34,21 @@ class App extends React.Component {
     console.debug("todo added with id: " + todoId);
   }
 
+  handleTodoDelete(todoId) {
+    let foundIndex = this.findTodoById(todoId);
+    if (foundIndex !== -1) {
+      this.state.todoList.splice(foundIndex, 1);
+      this.setState({ todoList: this.state.todoList });
+      console.debug("deleted todo with id: " + todoId);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Todo List</h1>
         <AddForm onTodoChange={this.handleTodoChange} />
-        <TodoList todoList={this.state.todoList} />
+        <TodoList todoList={this.state.todoList} onTodoDelete={this.handleTodoDelete} />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 class TodoRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.deleteSelf = this.deleteSelf.bind(this);
+  }
+  deleteSelf() {
+    this.props.onTodoDelete(this.props.id);
+  }
   render() {
     return (
-      <li>{this.props.title}</li>
+      <li>{this.props.title} <button onClick={this.deleteSelf}>Delete</button></li>
     );
   }
 }
@@ -12,7 +19,7 @@ class TodoList extends React.Component {
   render() {
     return (
       <ul>
-        {this.props.todoList.map(item => <TodoRow key={item.id} title={item.title} />)}
+        {this.props.todoList.map(item => <TodoRow key={item.id} id={item.id} title={item.title} onTodoDelete={this.props.onTodoDelete} />)}
       </ul>
     );
   }
